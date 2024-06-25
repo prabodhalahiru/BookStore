f969a29cad8854a49021e660278c7452c9f66743
Lahiru <prabodhalahiru119@gmail.com>
2024-06-25 23:34:50 +0530

    Books DB updated

--- BookStoreMainSup/Controllers/BooksController.cs
+++ BookStoreMainSup/Controllers/BooksController.cs
@@ -1,6 +1,26 @@
-﻿namespace BookStoreMainSup.Controllers
+﻿using BookStoreMainSup.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStoreMainSup.Models;
+
+namespace BookStoreMainSup.Controllers
 {
-    public class BooksController
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Books>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
     }
 }
--- BookStoreMainSup/Program.cs
+++ BookStoreMainSup/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     )); 
