946203f053f9c4d91e6ba17bef2d10810dc9199d
Dimusha <dimusha.perera@gmail.com>
2024-07-17 18:17:53 +0530

    Admin part added

---
+++ BookStoreMainSup/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsync("An unexpected error occurred: " + ex.Message);
+        }
+    }
+}
