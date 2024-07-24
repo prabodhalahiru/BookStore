4ea094c0bedc6232d371acf8b818f8d8bf2885ae
heshannammunige <nammunigeheshan@gmail.com>
2024-07-24 18:13:28 +0530

    Added Required fields to Model

--- BookStoreMainSup/Models/Books.cs
+++ BookStoreMainSup/Models/Books.cs
@@ -1,11 +1,21 @@
-﻿namespace BookStoreMainSup.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreMainSup.Models
 {
     public class Books
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Author { get; set; }
+
+        [Required]
         public double Price { get; set; }
+
+        [Required]
         public long isbn { get; set; }
     }
 }
